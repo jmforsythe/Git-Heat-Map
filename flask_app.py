f7a7a9b6fe0c18349ef1a62862531f8ea8081fe1
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2023-01-28 00:34:37 +0000

    Add index page

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
 import databaseToJSON
 import databaseToJSONFiltered
+import glob
 
 app = Flask(__name__)
 app.static_folder = "static"
+
+@app.route("/")
+def index_page():
+    db_list = [db[:-3] for db in glob.glob("*.db")]
+    return render_template("index.html", db_list=db_list)
 
 @app.route("/<name>")
 def treemap_page(name):
