99fb73cc21d9646eff673881b2a161ac9dde0c2a
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2023-02-12 16:09:37 +0000

    Reorder functions

--- static/javascript/sidebar.js
+++ static/javascript/sidebar.js
@@ -1,14 +1,3 @@
-function back_button_setup() {
-    let back_button = document.getElementById("back_button")
-    if (back_button) {
-        back_button.onclick = () => {
-            path = back_stack.pop()
-            if (path == null) path = ""
-            display_filetree_path(filetree_obj_global, highlighting_obj_global, path, get_hue())
-        }
-    }
-}
-
 function make_list_item(text) {
     let el = document.createElement("div")
     el.classList.add("list_item")
@@ -79,16 +68,12 @@ function get_include_exclude(filter_name, filter_id) {
     return out
 }
 
-function submit_query_setup() {
-    const query_list = [["email", "email_filter"], ["commit", "commit_filter"], ["filename", "filename_filter"], ["datetime", "datetime_filter"]]
-    let submit_button = document.getElementById("submit_query")
-    if (submit_button) {
-        submit_button.onclick = () => {
-            let query = {}
-            query_list.forEach((q) => query = {...query, ...get_include_exclude(...q)})
-            display_filetree_with_params({}, query, get_hue())
-        }
-    }
+function size_picker_setup() {
+    let el = document.getElementById("size_picker_number")
+    if (MIN_AREA != -Infinity) el.value = MIN_AREA
+    el.addEventListener("input", (event) => {
+        MIN_AREA = el.value
+    })
 }
 
 function color_picker_setup() {
@@ -113,12 +98,27 @@ function get_hue() {
     return input_number.value
 }
 
-function size_picker_setup() {
-    let el = document.getElementById("size_picker_number")
-    if (MIN_AREA != -Infinity) el.value = MIN_AREA
-    el.addEventListener("input", (event) => {
-        MIN_AREA = el.value
-    })
+function submit_query_setup() {
+    const query_list = [["email", "email_filter"], ["commit", "commit_filter"], ["filename", "filename_filter"], ["datetime", "datetime_filter"]]
+    let submit_button = document.getElementById("submit_query")
+    if (submit_button) {
+        submit_button.onclick = () => {
+            let query = {}
+            query_list.forEach((q) => query = {...query, ...get_include_exclude(...q)})
+            display_filetree_with_params({}, query, get_hue())
+        }
+    }
+}
+
+function back_button_setup() {
+    let back_button = document.getElementById("back_button")
+    if (back_button) {
+        back_button.onclick = () => {
+            path = back_stack.pop()
+            if (path == null) path = ""
+            display_filetree_path(filetree_obj_global, highlighting_obj_global, path, get_hue())
+        }
+    }
 }
 
 function export_svg_setup() {
@@ -136,14 +136,14 @@ function export_svg_setup() {
 }
 
 function main() {
-    back_button_setup()
     filter_entry_setup("email_filter")
     filter_entry_setup("commit_filter")
     filter_entry_setup("filename_filter")
     date_entry_setup("datetime_filter")
-    submit_query_setup()
-    color_picker_setup()
     size_picker_setup()
+    color_picker_setup()
+    submit_query_setup()
+    back_button_setup()
     export_svg_setup()
 }
 
