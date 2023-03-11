f322021077a7eaa4276c6a4865559b0fefbb620a
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2023-03-11 01:45:40 +0000

    Add client side caching

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import pathlib
 import functools
 import sqlite3
+import functools
 
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, make_response
 
 import databaseToJSON
 
@@ -20,19 +21,36 @@ def query_list():
     return (d.stem for d in query_dir.iterdir())
 
 def valid_db_check(func):
-    def wrapper(name):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        name = kwargs["name"]
         if name in db_list():
-            return func(name)
+            return func(*args, **kwargs)
         abort(404)
-    wrapper.__name__ = func.__name__
     return wrapper
 
-def valid_db_and_query_check(func):
-    def wrapper(name, query):
-        if name in db_list() and query in query_list():
-            return func(name, query)
+def valid_query_check(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs["query"]
+        if query in query_list():
+            return func(*args, **kwargs)
         abort(404)
-    wrapper.__name__ = func.__name__
+    return wrapper
+
+def cache_on_db_change(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        name = kwargs["name"]
+        db_path = (repos_dir / name / name).with_suffix(".db")
+        server_time = db_path.stat().st_mtime
+        client_time = request.if_modified_since
+        if client_time != None and client_time.timestamp()+1 >= server_time:
+            return make_response("", 304)
+        rv = make_response(func(*args, **kwargs))
+        rv.headers["cache-control"] = "no-cache"
+        rv.last_modified = db_path.stat().st_mtime
+        return rv
     return wrapper
 
 @app.route("/")
@@ -45,6 +63,7 @@ def treemap_page(name):
     return render_template("treemap.html", name=name)
 
 @app.route("/<name>/filetree.json")
+@cache_on_db_change
 @valid_db_check
 def filetree_json(name):
     db_path = (repos_dir / name / name).with_suffix(".db")
@@ -59,6 +78,7 @@ def filetree_json(name):
             return f.read().decode(errors="replace")
 
 @app.route("/<name>/highlight.json")
+@cache_on_db_change
 @valid_db_check
 def highight_json(name):
     valid_keys = ("email_include", "email_exclude", "commit_include", "commit_exclude", "filename_include", "filename_exclude", "datetime_include", "datetime_exclude")
@@ -67,17 +87,18 @@ def highight_json(name):
     return get_highlight_json(name, params_frozen)
 
 @app.route("/<name>/query/<query>")
-@valid_db_and_query_check
+@cache_on_db_change
+@valid_db_check
+@valid_query_check
 def sql_query(name, query):
     db_path = (repos_dir / name / name).with_suffix(".db")
     query_path = (project_dir / "sql" / query).with_suffix(".sql")
     with open(query_path) as f:
-        query = f.read()
+        query_text = f.read()
         con = sqlite3.connect(db_path)
         cur = con.cursor()
-        cur.execute(query)
+        cur.execute(query_text)
         out = cur.fetchall()
-        con.close()
         return out
 
 @functools.lru_cache(maxsize=100)
