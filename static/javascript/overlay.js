b7ae8a9f2eee54b05bf36d63394ac5ae905e6269
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2023-07-22 02:46:44 +0100

    Add title to table

--- static/javascript/overlay.js
+++ static/javascript/overlay.js
@@ -58,7 +58,10 @@ function sql_repsonse_to_table(r) {
 }
 
 async function update_info_box_with_file_stats(path) {
-    const response = await get_file_stats(path)
-    //console.log(response)
-    set_info_content(sql_repsonse_to_table(response))
+    const h1 = document.createElement("h1")
+    h1.appendChild(document.createTextNode("Author emails with most changes to"))
+    const h2 = document.createElement("h2")
+    h2.appendChild(document.createTextNode(path))
+    const table = sql_repsonse_to_table(await get_file_stats(path))
+    set_info_content(h1, h2, table)
 }
