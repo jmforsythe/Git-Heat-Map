4fef95274160c1c8ec2d7f3964fa67206f2062c5
Jonathan Forsythe <72832948+jmforsythe@users.noreply.github.com>
2023-11-09 18:30:59 +0000

    Fix author stats not loading

--- static/javascript/overlay.js
+++ static/javascript/overlay.js
@@ -145,7 +145,7 @@ async function update_info_box_with_author_stats(author_email) {
     const h2 = document.createElement("h2")
     h2.appendChild(document.createTextNode(author_email))
     update_info_box_downloading()
-    const response = await get_author_stats(hash)
+    const response = await get_author_stats(author_email)
     update_info_box_rendering()
     const table = sql_response_to_table(await response.json())
     set_info_content(h1, h2, table)
